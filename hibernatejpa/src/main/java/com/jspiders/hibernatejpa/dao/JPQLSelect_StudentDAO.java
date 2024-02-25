5e8f92a1ce76f42fad384af7c8585a5939f6b949
Arti12671 <artiudage@gmail.com>
2024-02-25 12:51:35 +0530

    commit

---
+++ hibernatejpa/src/main/java/com/jspiders/hibernatejpa/dao/JPQLSelect_StudentDAO.java
@@ -0,0 +1,58 @@
+package com.jspiders.hibernatejpa.dao;
+
+import java.sql.Connection;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import com.jspiders.hibernatejpa.dto.StudentDTO;
+
+public class JPQLSelect_StudentDAO {
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+	private static EntityTransaction entityTransaction;
+	
+	public static void main(String[] args) {
+		openConnection();
+		entityTransaction.begin();
+		
+		List<StudentDTO> student = findAll();
+		for (StudentDTO studentDTO : student) {
+			System.out.println(studentDTO);
+		}
+		
+		entityTransaction.commit();
+		closeConnection();
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static List<StudentDTO> findAll() {
+		Query query = entityManager.createQuery("SELECT student FROM StudentDTO student ");
+		List student = query.getResultList();
+		return student;
+	}
+	
+	private static void openConnection() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("student");
+		entityManager = entityManagerFactory.createEntityManager();
+		entityTransaction = entityManager.getTransaction();
+	}
+	
+	public static void closeConnection() {
+		if (entityManagerFactory != null) {
+			entityManagerFactory.close();
+		}
+		if (entityManager != null) {
+			entityManager.close();
+		}
+		if (entityTransaction != null) {
+			if (entityTransaction.isActive()) {
+				entityTransaction.rollback();
+			}
+		}
+	}
+}
